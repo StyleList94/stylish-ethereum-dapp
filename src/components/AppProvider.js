9f10a55d69cc2195f810abdedbee19376afaa837
StyleList94 <43318609+StyleList94@users.noreply.github.com>
2023-01-04 01:28:04 +0900

    feat: update app provider

--- src/components/AppProvider.js
+++ src/components/AppProvider.js
@@ -1,7 +1,14 @@
 import { Global } from '@emotion/react';
-import { configureChains, chain, WagmiConfig, createClient } from 'wagmi';
+import { configureChains, WagmiConfig, createClient } from 'wagmi';
+import {
+  mainnet,
+  goerli,
+  polygon,
+  polygonMumbai,
+  bsc,
+  bscTestnet,
+} from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
 import { ToastContainer } from 'react-toastify';
@@ -15,58 +22,10 @@ import RouteProgress from '@/components/RouteProgress';
 
 import 'react-toastify/dist/ReactToastify.css';
 
-const bscChain = {
-  id: 56,
-  name: 'BNB Smart Chain Mainnet',
-  network: 'binance',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'BNB',
-    symbol: 'BNB',
-  },
-  rpcUrls: {
-    default: 'https://bsc-dataseed1.binance.org/',
-  },
-  blockExplorers: {
-    default: { name: 'BSCScan', url: 'https://bscscan.com/' },
-  },
-  testnet: false,
-};
-
-const bscTestnetChain = {
-  id: 97,
-  name: 'BNB Smart Chain Testnet',
-  network: 'binanceTestnet',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'tBNB',
-    symbol: 'tBNB',
-  },
-  rpcUrls: {
-    default: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
-  },
-  blockExplorers: {
-    default: { name: 'BSCScan', url: 'https://testnet.bscscan.com/' },
-  },
-  testnet: true,
-};
-
 const { chains, provider, webSocketProvider } = configureChains(
-  [
-    chain.mainnet,
-    chain.rinkeby,
-    chain.polygon,
-    chain.polygonMumbai,
-    bscChain,
-    bscTestnetChain,
-  ],
-  [
-    jsonRpcProvider({
-      rpc: (chain) => ({ http: chain.rpcUrls.default }),
-    }),
-    publicProvider(),
-  ],
-  { targetQuorum: 2 },
+  [mainnet, goerli, polygon, polygonMumbai, bsc, bscTestnet],
+  [publicProvider()],
+  { targetQuorum: 1 },
 );
 
 const client = createClient({
