0308ed7a48e6769cee6f868396471257c511c1ef
StyleList94 <43318609+StyleList94@users.noreply.github.com>
2023-01-10 23:19:48 +0900

    feat: update ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,364 @@
+import { useState } from 'react';
 import Head from 'next/head';
+import styled from '@emotion/styled';
+import {
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useNetwork,
+  usePrepareSendTransaction,
+  useSendTransaction,
+  useSignMessage,
+  useSwitchNetwork,
+} from 'wagmi';
+import { Button, CircularProgress, TextField } from '@mui/material';
+import { parseEther } from 'ethers/lib/utils';
+
+import useMounted from '../hooks/useMounted';
+
+const PageContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  padding: 3rem;
+`;
+
+const PageTitle = styled.h1`
+  font-family: 'DM Sans', sans-serif;
+  font-weight: normal;
+  font-size: 1.5rem;
+`;
+
+const CardSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 375px;
+  padding: 1rem;
+  border: 1px solid #e9ecef;
+  border-radius: 12px;
+  box-shadow: 5px 1px 10px 0 rgba(206, 212, 218, 0.2);
+`;
+
+const CardTitle = styled.h2`
+  margin-bottom: 1rem;
+  font-family: 'Poppins', sans-serif;
+  font-weight: normal;
+  font-size: 1.25rem;
+  text-align: center;
+`;
+
+const CardContentList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+  padding: 1rem 0;
+`;
+
+const CardContentItem = styled.section`
+  h3 {
+    font-family: 'Inter', sans-serif;
+    font-weight: normal;
+    font-size: 1rem;
+    color: #adb5bd;
+  }
+
+  p {
+    overflow-x: auto;
+    margin-top: 0.25rem;
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.25rem;
+
+    &::-webkit-scrollbar {
+      background-color: #fff;
+      height: 8px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background-color: rgba(206, 212, 218, 0.5);
+      border-radius: 24px;
+    }
+  }
+`;
+
+const CardActionGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1rem;
+  border: 1px solid #f1f3f5;
+  border-radius: 12px;
+`;
+
+const CardResultBox = styled.div`
+  h3 {
+    font-family: 'Inter', sans-serif;
+    font-weight: normal;
+    font-size: 1rem;
+    color: #adb5bd;
+  }
+
+  p {
+    overflow-x: auto;
+    margin-top: 0.25rem;
+    font-family: 'Roboto', sans-serif;
+    font-size: 0.8755rem;
+
+    &::-webkit-scrollbar {
+      background-color: #fff;
+      height: 8px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background-color: rgba(206, 212, 218, 0.5);
+      border-radius: 24px;
+    }
+  }
+`;
+
+const ErrorContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+
+  p {
+    font-family: 'Roboto', sans-serif;
+    font-size: 0.875rem;
+    color: #fa5252;
+  }
+`;
 
 export default function IndexPage() {
+  const {
+    connect,
+    connectors,
+    isLoading: isConnecting,
+    error: errorConnect,
+    pendingConnector,
+  } = useConnect();
+  const { disconnect } = useDisconnect();
+  const {
+    status,
+    address,
+    connector: activeConnector,
+    isConnected,
+  } = useAccount();
+  const { chain } = useNetwork();
+  const {
+    switchNetwork,
+    chains,
+    status: switchNetworkStatus,
+    error: errorSwitch,
+  } = useSwitchNetwork();
+  const {
+    signMessage,
+    status: signMessageStatus,
+    data: signMessageData,
+    error: errorSignMessage,
+  } = useSignMessage();
+
+  const [isReadyToSend, setIsReadyToSend] = useState(false);
+
+  const [toInput, setToInput] = useState('');
+  const [valueInput, setValueInput] = useState('');
+
+  const isMounted = useMounted();
+  const { config } = usePrepareSendTransaction({
+    request: {
+      to: toInput,
+      value: parseEther(valueInput || '0'),
+    },
+    enabled: isReadyToSend,
+  });
+
+  const {
+    sendTransaction,
+    status: txStatus,
+    data: txResult,
+    error: errorSendTx,
+  } = useSendTransaction(config);
+
   return (
-    <div>
-      <h1>Stylish Ethereum DApp</h1>
-    </div>
+    <PageContainer>
+      <PageTitle>Stylish Ethereum DApp</PageTitle>
+      <CardSection>
+        <CardTitle>Account</CardTitle>
+        <CardContentList>
+          <CardContentItem>
+            <h3>Status</h3>
+            <p>{status}</p>
+          </CardContentItem>
+          {address && (
+            <CardContentItem>
+              <h3>Address</h3>
+              <p>{address}</p>
+            </CardContentItem>
+          )}
+          {activeConnector && (
+            <CardContentItem>
+              <h3>Connector</h3>
+              <p>{activeConnector.name}</p>
+            </CardContentItem>
+          )}
+        </CardContentList>
+
+        {isMounted && (
+          <CardContentItem>
+            <h3>Connect</h3>
+            <CardActionGroup>
+              {!isConnected &&
+                connectors.map((connector) => (
+                  <Button
+                    key={`connector-${connector.id}`}
+                    onClick={() => connect({ connector })}
+                    disabled={
+                      isConnecting && pendingConnector.id === connector.id
+                    }
+                  >
+                    {connector.id}({connector.name})
+                    {isConnecting && pendingConnector.id === connector.id && (
+                      <CircularProgress size={16} />
+                    )}
+                  </Button>
+                ))}
+              {isConnected && <Button onClick={disconnect}>Disconnect</Button>}
+            </CardActionGroup>
+          </CardContentItem>
+        )}
+        {errorConnect && (
+          <ErrorContent>
+            <p>{errorConnect.name}</p>
+            <p>{errorConnect.message}</p>
+          </ErrorContent>
+        )}
+      </CardSection>
+      {isMounted && isConnected && (
+        <CardSection>
+          <CardTitle>Network</CardTitle>
+          <CardContentList>
+            <CardContentItem>
+              <h3>Chain Id</h3>
+              <p>{chain?.id}</p>
+            </CardContentItem>
+            <CardContentItem>
+              <h3>Name</h3>
+              <p>{chain?.name}</p>
+            </CardContentItem>
+            <CardContentItem>
+              <h3>Switch Network Status</h3>
+              <p>{switchNetworkStatus}</p>
+            </CardContentItem>
+          </CardContentList>
+          <CardContentItem>
+            <h3>Switch To</h3>
+            <CardActionGroup>
+              {chains.map((chain) => (
+                <Button
+                  key={`${chain.id}-${chain.name}`}
+                  onClick={() => switchNetwork?.(chain.id)}
+                >
+                  {chain.name}
+                </Button>
+              ))}
+            </CardActionGroup>
+          </CardContentItem>
+          {errorSwitch && (
+            <ErrorContent>
+              <p>{errorSwitch.name}</p>
+              <p>{errorSwitch.message}</p>
+            </ErrorContent>
+          )}
+        </CardSection>
+      )}
+      {isMounted && isConnected && (
+        <CardSection>
+          <CardTitle>Sign Message Test</CardTitle>
+          <CardContentList>
+            <CardContentItem>
+              <h3>Status</h3>
+              <p>{signMessageStatus}</p>
+            </CardContentItem>
+            <CardActionGroup>
+              <Button onClick={() => signMessage({ message: 'hello world!' })}>
+                Sign Message
+              </Button>
+            </CardActionGroup>
+          </CardContentList>
+          <CardContentList>
+            {signMessageData && (
+              <CardResultBox>
+                <h3>Result</h3>
+                <p>{signMessageData}</p>
+              </CardResultBox>
+            )}
+          </CardContentList>
+          {errorSignMessage && (
+            <ErrorContent>
+              <p>{errorSignMessage.name}</p>
+              <p>{errorSignMessage.message}</p>
+            </ErrorContent>
+          )}
+        </CardSection>
+      )}
+      {isMounted && isConnected && (
+        <CardSection>
+          <CardTitle>Transaction Test</CardTitle>
+          <CardContentList>
+            <CardContentItem>
+              <h3>Status</h3>
+              <p>{txStatus}</p>
+            </CardContentItem>
+            <CardContentItem>
+              <h3>Send</h3>
+              <CardActionGroup>
+                <TextField
+                  id="tx-to"
+                  label="To"
+                  name="to"
+                  type="text"
+                  variant="standard"
+                  onChange={(e) => setToInput(e.target.value)}
+                />
+                <TextField
+                  id="tx-ether"
+                  label="Ether"
+                  name="ether"
+                  type="text"
+                  variant="standard"
+                  onChange={(e) => setValueInput(e.target.value)}
+                />
+                <Button
+                  onClick={() => {
+                    setIsReadyToSend(true);
+                    sendTransaction?.();
+                  }}
+                >
+                  Send Ether
+                </Button>
+              </CardActionGroup>
+            </CardContentItem>
+            <CardContentList>
+              {txResult && (
+                <>
+                  <CardResultBox>
+                    <h3>Tx Hash</h3>
+                    <p>{txResult.hash}</p>
+                  </CardResultBox>
+                </>
+              )}
+            </CardContentList>
+          </CardContentList>
+          {errorSendTx && (
+            <ErrorContent>
+              <p>{errorSendTx.name}</p>
+              <p>{errorSendTx.message}</p>
+            </ErrorContent>
+          )}
+        </CardSection>
+      )}
+    </PageContainer>
   );
 }
 
