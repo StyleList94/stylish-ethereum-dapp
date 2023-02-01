bd2caebd707e5b55c615bfefafd96bd277412900
StyleList94 <43318609+StyleList94@users.noreply.github.com>
2023-02-02 01:43:39 +0900

    feat: apply svg component

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
     config.resolve.fallback = { fs: false };
 
     return config;
--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -9,3 +9,17 @@ declare global {
     klaytn: IpcProvider & Klaytn;
   }
 }
+
+/* image module */
+declare module '*.png';
+declare module '*.jpeg';
+declare module '*.jpg';
+
+/* svg component */
+declare module '*.svg' {
+  import React from 'react';
+
+  export const ReactComponent: React.FC<React.SVGProps<SVGElement>>;
+
+  export default ReactComponent;
+}
